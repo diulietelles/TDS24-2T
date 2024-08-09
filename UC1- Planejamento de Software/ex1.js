709bc2671be753a79b09f2c0e3847b2a7bf15133
Diulie Telles <121645164+diulietelles@users.noreply.github.com>
2024-08-09 15:18:41 -0300

    Add files via upload

---
+++ UC1- Planejamento de Software/ex1.js
@@ -0,0 +1,13 @@
+/* Requisitos funcionais: Cafeteria
+
+Gestão de pedidos: permitir registros de pedidos de clientes, itens, remover ou adicionais ingredientes, gerar pedidos para que usuários consigam visualizar e preparar pedidos(cliente, mesa que vai receber o pedido)
+
+Controle de estoque : atualizar automaticamente o estoque conforme os itens vendidos, alerta usuários conforme itens vendidos e baixo estoque
+
+Relatório de vendas: deve atualizar diariamente, semanalmente, detalhando produtos e períodos de vendas
+
+integrar sistemas de pagamentos, credito debito, QRcode, registrar histórico, cardápio ajuda(para mulheres em risco)
+
+Cadastro de clientes: nome, histórico da compra
+
+Feedback do usuário(colaborador da empresa que utiliza o sistema para realizar os pedidos dos clientes): sugestões, reclamações */
