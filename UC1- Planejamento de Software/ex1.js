85a9d048f7fc9f25e90d818917b755fc42d9feba
Diulie Telles <121645164+diulietelles@users.noreply.github.com>
2024-08-09 16:41:13 -0300

    ex1.js

--- UC1- Planejamento de Software/ex1.js
+++ UC1- Planejamento de Software/ex1.js
@@ -1,4 +1,4 @@
-/* Requisitos funcionais: Cafeteria
+/* **Requisitos funcionais: Cafeteria
 
 Gestão de pedidos: permitir registros de pedidos de clientes, itens, remover ou adicionais ingredientes, gerar pedidos para que usuários consigam visualizar e preparar pedidos(cliente, mesa que vai receber o pedido)
 
@@ -10,6 +10,20 @@ integrar sistemas de pagamentos, credito debito, QRcode, registrar histórico, c
 
 Cadastro de clientes: nome, histórico da compra
 
-Feedback do usuário(colaborador da empresa que utiliza o sistema para realizar os pedidos dos clientes): sugestões, reclamações */
+Feedback do usuário(colaborador da empresa que utiliza o sistema para realizar os pedidos dos clientes): sugestões, reclamações 
+
+**Requisitos Não funcionais:
+
+1- Desempenho: o software deve processar pedidos e transações em menos de 2 segundos, o sistema deve suportar até 100 pedidos simultaneamente, sem queda no seu desempenho
+
+2- Usabilidade: A interface deve ser intuitiva e facil para utilizar, design responsivo(adaptado para varios formatos de dispositivos)
+
+3- Segurança: O software deve proteger os dados dos usuarios e das transações(compras) - Criptografia, exigir autentificação para setores administrativos
+
+4- Confiabilidade: O software deve ter uptime de 99,9% minimizando interrupçoes nos seus serviços, deve realizar backups automaticos diatriamente para evitar perda de dados 
+
+5- Compatibilidade: O software deve ser compativel com a m,aioria dos sistemas operacionais usados no gerenciamento da cafeteria
+
+7- Adicionais para clientes: tablet na mesa dos clientes mostrando promoçoes de combos na cafeteria e fornecer livros online no proprio sistema para os clientes lerem enquanto tomam café
 
 Nomes: Diulie, Freliannys e Leandra
