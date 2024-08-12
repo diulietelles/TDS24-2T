e1254cbc3ba52c3639c77bb1908a579bef855be2
diulietelles <tellesdiulie@gmail.com>
2024-08-12 16:45:58 -0300

    adicionar arquivos

---
+++ UC2-Desenvolver Algoritmos/variaveis.js
@@ -0,0 +1,22 @@
+const nome= 'Diulie'
+const sobrenome = 'Telles'
+const idade = 29
+const estudante = 'sim'
+
+console.log('Meu nome é:', nome, sobrenome, 'e eu tenho:',idade, 'anos',',sou estudante?', estudante);
+
+const got = 'Game of Thrones'
+const temporadasDeGot = '8'
+
+console.log(typeof got) 
+console.log(typeof temporadasDeGot)
+
+
+let novaVariavel
+console.log(typeof novaVariavel)
+
+novaVariavel = 2
+console.log(typeof novaVariavel)
+
+novaVariavel = undefined
+console.log(typeof novaVariavel)
