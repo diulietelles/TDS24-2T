51057d53a8387f7c28b172e5ff7237fc9a1a7505
Diulie Telles <121645164+diulietelles@users.noreply.github.com>
2024-08-14 16:57:55 -0300

    Add files via upload

---
+++ UC5- Inglês/aula1.js
@@ -0,0 +1,36 @@
+/*Exercício 1
+1: Positivos:
+- criar vínculos à distância.
+- Acesso rápido à informações e aprendizagem 
+- Oportunidades de networking.
+
+Negativos:
+- Ansiedade e estresse
+- Comparação social que afeta a autoestima.
+- Distrações que prejudica a produtividade.
+
+
+ 2-Impacto positivo:
+- Campanhas de conscientização sobre saúde mental.
+- Movimentos sociais como #MeToo e Black Lives Matter.
+- doações e apoio a causas em geral .
+
+3- Saudável x Excessivo
+- Uso Saudável: Equilíbrio entre o tempo online e offline
+- Uso Excessivo: Dependência das redes para validação, ansiedade ao estar desconectado, e impacto negativo no sono e nas atividades diárias.
+
+4-: Estratégias para minimizar os impactos negativos: 
+- Definir horários específicos para uso das redes sociais.
+- Silenciar notificações para evitar distrações constantes.
+- Seguir perfis que promovam conteúdo positivo e inspirador.
+- estar presente no momento.
+- leitura e exercícios físicos.
+
+5: Plataforma Saudável:
+- Criação de recursos que incentivem pausas e atividades offline.
+- Filtros para conteúdos negativos ou que promovam comparações irreais.
+- Implementação de alertas de uso excessivo.
+- Incentivo a interações positivas, como reconhecimento de apoio e ajuda entre usuários.
+*/
+
+
