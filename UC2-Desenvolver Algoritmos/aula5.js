587231afc8095aaacd6e09d8c873dc9237db4c68
diulietelles <tellesdiulie@gmail.com>
2024-08-15 14:46:24 -0300

    adicionar arquivos

---
+++ UC2-Desenvolver Algoritmos/aula5.js
@@ -0,0 +1,31 @@
+
+//*****Exercicio
+let a = 10
+let b = 10
+console.log(b)
+b = 5
+console.log(a, b)
+
+/******Resposta
+1- define a e b com valor 10
+2- exibe o valor de b que é 10
+3- atribui 5 a b
+4- exibe os valores de a que continua 10 e b que agora é 5
+10
+10 5
+*/
+
+//*****Exercicio
+let a1 = 10
+let b2 = 20
+c = a
+b = c
+a = b
+console.log(a, b,c)
+
+/******Resposta
+1- let a=10, b=20 e c=a que é 10 e b=20
+2- c = a que é 10, não tem alterações
+ b atualiza para 10, pois o c é 10
+a = b que agora 10(sem mais alterações)
+10 10 10*/
