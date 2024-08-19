72e63c34cc92a9e8056d57e73bc45342faf239d2
diulietelles <tellesdiulie@gmail.com>
2024-08-19 15:24:47 -0300

    adicionar arquivos

--- UC2-Desenvolver Algoritmos/aula6-operadores.js
+++ UC2-Desenvolver Algoritmos/aula6-operadores.js
@@ -1,2 +1,52 @@
-//*************************Operadores aritmeticos**********************************//
+//************************* Operadores  Aritméticos ****************************//
 
+/* Exemplo 1:
+const primeiroValor = 10
+const segundoValor = 20
+const resultado = primeiroValor + segundoValor + 4
+console.log('O resultado da soma é: ', resultado)
+
+// Exemplo 2:
+const primeiroValor2 = 10
+const segundoValor2 = 20
+const resultado2 = primeiroValor2 - segundoValor2 
+console.log('O resultado da subtracao é: ', resultado2)
+
+// Exemplo 3:
+const primeiroValor3 = 10
+const segundoValor3 = 25
+const resultado3 = primeiroValor3 * segundoValor3
+console.log('O resultado da multiplicaçao é: ', resultado3)
+
+// Exemplo 4:
+const primeiroValor4 = 345
+const segundoValor4 = 10
+const resultado4 = primeiroValor4 / segundoValor4 
+console.log('O resultado da divisao é: ', resultado4)
+
+// Exemplo 5:
+const primeiroValor5 = 11%4
+console.log('O resultado do resto da divisao é: ', primeiroValor5)
+*/
+//Exercicios:
+//Somar 3 com 4:
+const primeiroValor = 3
+const segundoValor = 4
+const resultado = primeiroValor + segundoValor 
+console.log('O resultado da soma é: ', resultado)
+
+//Multiplicar 3 com 5 e dividir o resultado por 2:
+const primeiroValor2 = 3
+const segundoValor2 = 5
+const resultado2 = (primeiroValor2 * segundoValor2) /2
+console.log('O resultado da multiplicação é: ', resultado2)
+
+// Subtrair 5 de 4 e multiplicar o resultador por -1:
+const primeiroValor3 = 5
+const segundoValor3 = 4
+const resultado3 = (primeiroValor3 - segundoValor3) * -1
+console.log('O resultado da subtracao é: ', resultado3)
+
+// Determinar o resto da divisão de 234 por 5:
+const primeiroValor4 = 234%5
+console.log('O resultado do resto da divisao é: ', primeiroValor4)
