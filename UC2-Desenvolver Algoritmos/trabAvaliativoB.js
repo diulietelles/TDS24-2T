19d169abd067423d3bdec7c2de980d38e30a357c
diulietelles <tellesdiulie@gmail.com>
2024-09-17 14:36:54 -0300

    adicionar arquivos

--- UC2-Desenvolver Algoritmos/trabAvaliativoB.js
+++ UC2-Desenvolver Algoritmos/trabAvaliativoB.js
@@ -1,6 +1,24 @@
 //************************* Gerenciador de Personagens de LotR ****************************//
 
 
+//Instruções do trabalho:
+/*Criação de Personagens:
+Utilize um array para armazenar os personagens.
+Cada personagem deve ser representado por um objeto com as seguintes propriedades:
+nome, classe, nivel, hp (pontos de vida), habilidades, e historicoAcoes.
+Ações dos Personagens:
+Cada personagem pode realizar ações como atacar, defender e usar habilidades especiais.
+As ações realizadas devem ser registradas no historicoAcoes.
+Histórico de Ações:
+O histórico de ações será armazenado no objeto do personagem. Sempre que o
+personagem realizar uma ação, o histórico deverá ser atualizado.
+Busca e Ordenação de Personagens:
+A função de busca deve percorrer o array de personagens e encontrar o personagem
+correspondente.
+A listagem de personagens deve ser feita em ordem alfabética. */
+
+
+
 
 
 // array para cadastrar personagens:
@@ -17,4 +35,25 @@ function cadastrarPersonagem(nome, classe, nivel, hp, habilidades) {
     historicoAcoes: []
   }
 }
+
+// função Ações dos Personagens:
+
+
+// Histórico de Ações:
+
+
+
+// Busca de Personagens por Nome:
+
+
+
+// Exclusão de personagens:
+
+
+
+// Listas Todos os Personagens:
+
+
+
+// Verificar Status do Personagem:
 
