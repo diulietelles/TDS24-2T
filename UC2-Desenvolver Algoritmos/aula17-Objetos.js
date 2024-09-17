39ef43f25efd3c4a81135f03920f408dcc28e8e9
diulietelles <tellesdiulie@gmail.com>
2024-09-17 14:20:01 -0300

    adicionar arquivos

--- UC2-Desenvolver Algoritmos/aula17-Objetos.js
+++ UC2-Desenvolver Algoritmos/aula17-Objetos.js
@@ -1,1 +1,58 @@
-//homework
+//************************* OBJETOS ****************************//
+
+/*
+const professor = {                                                    // variavel professor= objeto
+    nome:'Maria',                                                     // variaveis maria idade, tarefa = chave
+    idade: 28,                                                       // strings e number que foram atribuidas as chaves = valor
+    tarefas:['dar aula','responder duvidas'],
+    contarPiada:function(){
+    }
+
+}
+console.log('e pa ve ou pa come?')
+
+
+
+*/
+// imprime o email do prof:
+const professor = {
+    nome: 'Lucas',
+    idade: 32,
+    email: 'lucas.g.f.alves@gmail.com'
+}    
+
+console.log(professor['email'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ UC2-Desenvolver Algoritmos/trabAvaliativoB.js
@@ -0,0 +1,20 @@
+//************************* Gerenciador de Personagens de LotR ****************************//
+
+
+
+
+// array para cadastrar personagens:
+let personagens = []
+
+// Função para cadastro de Personagens:
+function cadastrarPersonagem(nome, classe, nivel, hp, habilidades) {
+  let personagem = {
+    nome: nome,
+    classe: classe,
+    nivel: nivel,
+    hp: hp,
+    habilidades: habilidades,
+    historicoAcoes: []
+  }
+}
+
