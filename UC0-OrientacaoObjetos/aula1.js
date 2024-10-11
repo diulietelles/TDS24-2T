f737dcd589b9d8a1ab788362e25b816edce52fc1
diulietelles <tellesdiulie@gmail.com>
2024-10-11 16:19:54 -0300

    add

---
+++ UC0-OrientacaoObjetos/aula1.js
@@ -0,0 +1,11 @@
+/*
+Pesquisar Linguagens orientadas a objetos mais utilizadas:
+
+
+Python: simples e versatil 
+Java: grandes sistemas corporativos
+C#: Microsoft e  desenvolvimento de jogos
+C++: jogos e softwares complexos
+Ruby: desenvolvimento web com framework(conjunto de ferramentas e bibliotecas)
+
+*/
---
+++ UC0-OrientacaoObjetos/aula2.js
@@ -0,0 +1,94 @@
+
+/*
+ SISTEMAS DE GERENCIAMENTO DE UMA LOJA DE ROUPAS:
+
+class Pecas (entidade)
+     preco (atributo)
+     estoque
+     preco
+     tamanho
+     codigo
+     cor
+     genero
+
+class Clientes 
+    nome
+    cpf
+    dataNascimento
+    historicoCompras
+    E-mail
+    endereco
+
+
+class Vendas 
+    itens
+    totalCompras
+    relatorios
+    descontos
+    metodosPagamentos
+    historicoVendas
+
+
+
+
+SISTEMA DE RESERVA DE ASSENTOS EM UM CINEMA: 
+
+class Sessão:
+    filmeCartaz: filmeDisponivel horario, duracao
+    CadastroSessoes: listaFilmes
+    DisponibilidadeAssentos: comprados, reservados, disponiveis
+    InfoSalas: numerofileiras, numeroAssentos,  assentosEspeciais
+    categoria
+    sala
+
+
+class Reserva:
+    selecaooSessao: filme, horario e sala
+    escolhaAssentos 
+    confirmacaoReserva: informar dados do comprador
+    pagamento: online ou venda antecipada
+    cancelamentoReserva: estabelecer prazo
+
+
+class Relatório:
+    relatorioSalas: salas ocupadas
+    relatorioReservas: clientes
+    relatorioCancelamentos 
+    relatorio Financeiro: vendas, estornos e afins
+
+
+
+
+// SISTEMA DE RESERVA DE ASSENTOS EM UM HOSPITAL:
+
+
+
+
+
+class Paciente:
+    cadastro
+    historicoMedico
+    consultaPaciente
+    diagnostico
+    altaHospitalar (ou não)
+
+class Internações:
+     leitoDisponivel
+     monitoramentoInternacao
+     tratamento
+     altaHospitalar (ou nao)
+
+class Medicos:
+    cadastro
+    especializacoes
+    plantoes
+  
+
+class Estoque:
+    medicamentos
+    insumos
+    equipamentosMedicos
+    entradasSaida
+    nivelEstoque
+
+*/
