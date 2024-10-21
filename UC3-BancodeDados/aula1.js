3a8b2f12af3165ffa3acb9a0daee36738c6d56c1
diulietelles <tellesdiulie@gmail.com>
2024-10-21 14:48:51 -0300

    add

--- UC3-BancodeDados/aula1.js
+++ UC3-BancodeDados/aula1.js
@@ -1,1 +1,81 @@
-// email: BD24-2T-FULANO-assunto
+
+/*
+
+
+
+
+1970
+
+quem criou : edgar codd
+
+banco de dados colecao de dados
+
+sgbdsoftware  gerencia esses dados
+
+sistde banco de dados combina ambos
+
+armazenmaneto estruturado consistencia,integridade e seguranca
+
+nivel fisico nivel logico e nivel de visao
+
+maior eficiência e facilidade de acesso
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
