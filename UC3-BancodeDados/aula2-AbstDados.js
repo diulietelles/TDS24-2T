ef55eea8968695ed1cfa1ed03a902c15d337f505
diulietelles <tellesdiulie@gmail.com>
2024-10-22 14:13:16 -0300

    add

---
+++ UC3-BancodeDados/aula2-AbstDados.js
@@ -0,0 +1,81 @@
+// Abstracao de Dados
+
+/*
+o que é:
+modelo de dados que fornece uma utilizacao de representacao conceitual, pegar infos filtradas do mundo real e aplicar em um modelo,
+
+
+modelos de dados: 
+conceitual: alto nivel- retira os detalhes, qual entidade e caracteristicas
+logico: medio - colocar tipagem dominio e chaves(estrangeiras e primarias), ligacoes entre tabelas de banco de dados
+fisico: baixo - sem necessidade de usar um banco de dados, utiliza SGBD, tipagens que nao é de um banco que esta usando
+
+
+estrutura de banco de dados:
+HIERARQUICO: mais antigo, pimario, sem relacionamento entre elas
+REDE: tinha uma network das entidades, mas dependia do metodo implementado,confuso
+
+RELACIONAL: relaciona, conecta entre todas as infos e intidades, organizado
+
+
+Orientado a objetos: incluir imagens, videos, graficos, estrutura nova, combina objetos e mais demorado e caro
+
+BD relaciona:
+tabelas: objetos do mundo real
+coluna: atributos e caracteristicas
+tipo de dados: nome, caracter, data, dentro de uma tabeça
+
+STORED PROCEDURES: ADD FUNCOES 
+TRIGGERS: GATILHOS, ACIONA UMA OUTRA ACAO AUTOMATICAMENTE
+
+
+chaves primarias: ID
+
+Chave estrangeira: uma tabela relaciona e precisa de uma info de outra tabela
+
+Views: consultas e armazenar elas para pequisar sempre que quiser
+Indices: pra identificar onde melhor esta cada dado, onde parou onde pode buscar
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
