4a90102cb4f847e804337674bda07b285ce6efa4
Diulie Telles <121645164+diulietelles@users.noreply.github.com>
2024-11-12 13:52:35 -0300

    Add files via upload

---
+++ UC3-BancodeDados/questoes.js
@@ -0,0 +1,21 @@
+/*
+1) Em que ano o modelo relacional de banco de dados foi desenvolvido?
+        Em 1970-1972
+
+2) Quem foi considerado o 'pai dos bancos de dados'?
+        Ted Edgard Frank Codd
+
+3) Explique a diferença de um banco de daods, Sistema Gerenciador de Banco de dados e Sistema Banco de Dados.
+        -SGBD: postgresg/mysql
+        -BD: Tabelas/views
+        -SBD: é oq engloba os outros dois pontos
+
+4) Quais são as principais características de um banco de dados?
+        Escalabilidade, integridade, segurança
+
+5) Quais são os diferentes níveis de abstração em um banco de daods.
+        Baixo, médio e alto (físico, lógico, conceitual).
+
+6) Liste as vantagens de usar um banco de dados:
+        Segurança, tira conflito de redundancia de daods, restauraçao do banco
+*/
